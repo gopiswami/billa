a938d806b7b99a4c9745038b40c159a40518129e
gopiswami <billagopiswami830@gmail.com>
2024-09-26 15:51:49 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,7 @@
 package com.example;
 
-public class app {
-  public static void main(string[] args {
-           system.out.printin("hello, world");
-  }
-
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
